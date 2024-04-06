22dc05037a7b1fda002771f7c4a0220e683ce394
Vagharshak Samsonyan <vagosamsonyan@yahoo.com>
2024-04-07 02:53:05 +0400

    1 of 1 test passed

--- src/main/java/com/epam/mjc/InterfaceCreator.java
+++ src/main/java/com/epam/mjc/InterfaceCreator.java
@@ -1,8 +1,24 @@
 package com.epam.mjc;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class InterfaceCreator {
 
     public Operation<Integer> divideBy(Integer divider) {
-        throw new UnsupportedOperationException("You should implement this method.");
+        return (list) -> {
+            List<Integer> result = new ArrayList<>();
+            for (Integer num : list) {
+                result.add(num / divider);
+            }
+            return result;
+        };
+    }
+
+    public static void main(String[] args) {
+        InterfaceCreator creator = new InterfaceCreator();
+        Operation<Integer> operation = creator.divideBy(3);
+        List<Integer> result = operation.apply(List.of(3, 6, 9));
+        System.out.println(result);
     }
 }
